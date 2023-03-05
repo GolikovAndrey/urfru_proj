1aaa7845eb59e7b7b93a08a85b6435ee85fbf387
GolikovAndrey <and.golikoff@yandex.ru>
2023-03-05 15:26:56 +0500

    Added autodownload dataset from Google Drive (now project do not requires pre-created data folder), updated Readme.md file

--- data_creation.py
+++ data_creation.py
@@ -1,9 +1,24 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from google_drive_downloader import GoogleDriveDownloader as gdd
+
+
+gdd.download_file_from_google_drive(
+    file_id='10SEtiZHyxhboMkLEzPjUQ364-S6iTWlB',
+    dest_path='./data/dataset.csv'
+)
+
 
 train = pd.read_csv('data\dataset.csv')
 train = train[train['Country'] == 'France']
-X_train, X_val, y_train, y_val = train_test_split(train[['year', 'code', 'id', 'period']], train[['polution']], test_size = 0.25, random_state = 42)
+
+
+X_train, X_val, y_train, y_val = train_test_split(
+    train[['year', 'code', 'id', 'period']], 
+    train[['polution']], 
+    test_size = 0.25, 
+    random_state = 42
+)
 
 X_train.to_csv('temp_data\X_train.csv', index=False)
 X_val.to_csv('temp_data\X_val.csv', index=False)
