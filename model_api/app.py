be44445fc4d5625c0ac074b6ca0b81631d304ad7
DrWhats <hudorojckov.leva@gmail.com>
2023-06-11 20:37:26 +0500

    Сын Андрея

--- model_api/app.py
+++ model_api/app.py
@@ -4,12 +4,10 @@ import pickle
 from sklearn.linear_model import SGDRegressor
 
 
-
-
 app = FastAPI(debug=True)
 
 @app.post('/predict')
-async def predict(data: pd.DataFrame):
+async def predict(data):
     with open('pickle_model.pkl', 'rb') as file: 
         model: SGDRegressor = pickle.load(file)
         return model.predict(data)
